51f390304e50001e6aba7915edb6f14ae869b30e
Ikenna <127604100+BoyNamedIkenna@users.noreply.github.com>
2025-08-01 12:37:26 +0100

    Changed "landing" to "Landing"

--- app/routes.ts
+++ app/routes.ts
@@ -6,7 +6,7 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-  index("routes/landing.tsx"),
+  index("routes/Landing.tsx"),
   route("signup", "routes/Signup.tsx"),
   route("login", "routes/Login.tsx"),
 
